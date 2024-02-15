0c55a4cbf7546eadcbbbef4f814ce267474f62fd
dm <heydruavh@gmail.com>
2024-02-15 18:34:47 +0530

    fix bugs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
 AOS.init({
   duration: 1000,
 });
+
+const mobileMenu = document.querySelector(".header__menu-mobile");
+const dropdown = document.querySelector(".header__menu-mobile-dropdown");
+const dropdownLinks = dropdown.querySelectorAll("a");
+const closeDropdownBtn = dropdown.querySelector("button");
+
+console.log(mobileMenu);
+console.log(dropdown);
+console.log(dropdownLinks);
+console.log(closeDropdownBtn);
+
+mobileMenu.addEventListener("click", () => {
+  dropdown.classList.remove("hide-dropdown");
+  dropdown.classList.add("show-dropdown");
+});
+
+dropdownLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    dropdown.classList.remove("show-dropdown");
+    dropdown.classList.add("hide-dropdown");
+  });
+});
+
+closeDropdownBtn.addEventListener("click", () => {
+  dropdown.classList.remove("show-dropdown");
+  dropdown.classList.add("hide-dropdown");
+});
